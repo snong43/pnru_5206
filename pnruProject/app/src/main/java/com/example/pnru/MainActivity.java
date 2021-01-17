d07c3685728a360ff90195b0422255275e9d67c8
Suchart Sutthasinobol <snong43@gmail.com>
2021-01-17 15:49:14 +0700

    first commit

---
+++ pnruProject/app/src/main/java/com/example/pnru/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.pnru;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public Button btnShowData;
+    public Button btnAddData;
+    public Button btnShowGps;
+    public Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAddData = (Button)findViewById(R.id.btnInsert);
+        btnShowData = (Button)findViewById(R.id.btnShow);
+        btnShowGps = (Button)findViewById(R.id.btnGps);
+
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(view.getContext(), MenuInsertActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnShowData.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(view.getContext(), MenuShowActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnShowGps.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(view.getContext(), MenuGpsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+}
---
+++ pnruProject/app/src/main/java/com/example/pnru/MenuInsertActivity.java
@@ -0,0 +1,13 @@
+package com.example.pnru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_insert);
+    }
+}
