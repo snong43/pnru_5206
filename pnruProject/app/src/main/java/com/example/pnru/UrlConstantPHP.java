40a5be42d33c7245c00f338f11d42efa63a6908c
Suchart Sutthasinobol <snong43@gmail.com>
2021-01-17 22:08:31 +0700

    บอกวิธี run ใน localhost เครื่องตัวเอง ต้องเปลี่ยน IP Address ที่ class นี้

--- pnruProject/app/src/main/java/com/example/pnru/UrlConstantPHP.java
+++ pnruProject/app/src/main/java/com/example/pnru/UrlConstantPHP.java
@@ -1,5 +1,6 @@
 package com.example.pnru;
 
 public class UrlConstantPHP {
+    // สำหรับ Test เครื่องตัวเอง ไป comment line แล้ว พิมพ์ ipconfig แล้วเปลี่ยน IP นี้แทน ไม่งั้นจะ run โปรแกรมไม่ได้
     public final static String MY_LOCAL = "192.168.56.1";
 }
